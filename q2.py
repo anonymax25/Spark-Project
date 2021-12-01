756f2267b784ecf93206bc81c0e83c3ff8b36a91
Maxime <maxime.dharboulle@gmail.com>
2021-12-01 18:33:47 +0100

    question 2 done

---
+++ q2.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType
+from pyspark.sql.window import Window
+from pyspark.sql.functions import col, udf, when, desc, sum, dense_rank, lit
+
+spark = SparkSession \
+    .builder \
+    .appName("TP3-bis") \
+    .master("local[*]") \
+    .getOrCreate()
+
+commits_file = "data/full.csv"
+
+commits_df = spark.read.format("csv") \
+    .option("inferSchema", "true") \
+    .option("header", "true") \
+    .load(commits_file)
+
+commits_df.filter(commits_df.repo == "apache/spark") \
+    .groupBy("author") \
+    .count().withColumnRenamed("count","commits") \
+    .orderBy(col("commits").desc()) \
+    .filter(commits_df.author.isNotNull()) \
+    .limit(1) \
+    .show()
